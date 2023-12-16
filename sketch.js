3ec0da97f2d64757f732fe38a6de96d5dac6e533
asyeon <153973140+asyeon@users.noreply.github.com>
2023-12-16 14:52:55 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ let factor=100;
 let speedSlider;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(width, height);
   //Vector = (x,y,z) z:속도감 표현
-  speedslider=createSlider(0, 20, 5, 0.1);
+  speedSlider=createSlider(0, 20, 5, 0.1);
   for(let i=0; i<500; i++) {
   stars[i] = createVector(
     random(-width*factor, width*factor),
@@ -22,20 +22,21 @@ function draw() {
   //중앙으로 올 수 있게 원점 변경
   translate(width/2, height/2);
   for(let star of stars) {
-  let x=star.x/star.z; //star.z에의해 영향받음
-  let y=star.y/star.z;
-  let px=star.z/star.pz;
-  let py=star.y/star.pz;
-  let d = map(star.z, 0, 400, 10, 1); 
-  circle(x,y,d);
-    stroke(255);
-    line(x,y,px,py);
-   star.z -= speedSlider.value();       //빨라짐
-   if(star.z<1) {
-    star.x=random(-width*factor, width*factor);
-    star.y=random(-height*factor, height*factor);
-    star.z=400;
-    star.pz=400;
-    }
+    let x=star.x/star.z; //star.z에의해 영향받음
+    let y=star.y/star.z;
+    let px=star.x/star.pz;
+    let py=star.y/star.pz;
+    let d = map(star.z, 0, 400, 10, 1); 
+    circle(x,y,d);
+      stroke(255);
+      line(x,y,px,py);
+      star.pz=star.z;
+    star.z -= speedSlider.value();       //빨라짐
+    if(star.z<1) {
+      star.x=random(-width*factor, width*factor);
+      star.y=random(-height*factor, height*factor);
+      star.z=400;
+      star.pz=400;
+      }
   }
 }
